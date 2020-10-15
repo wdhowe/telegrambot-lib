dea442ee6ddd095ce6b6e172a5b77af2c46af862
wdhowe <wdhowe@protonmail.com>
2020-10-14 23:35:41 -0500

    issue/1/unit-tests: Initial Unit Tests
    Added initial unit tests for core, config, http namespaces.

--- test/telegrambot_lib/core_test.clj
+++ test/telegrambot_lib/core_test.clj
@@ -1,7 +1,15 @@
 (ns telegrambot-lib.core-test
   (:require [clojure.test :refer :all]
-            [telegrambot-lib.core :refer :all]))
+            [telegrambot-lib.core :as lib-core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest create-test-1
+  (testing "Test TBot instance creation - token."
+    (let [mybot (lib-core/create "123456")]
+      (is (= "123456" (:bot-token mybot)))
+      (is (= :info (:log-level mybot))))))
+
+(deftest create-test-2
+  (testing "Test TBot instance creation - token + log level."
+    (let [mybot (lib-core/create "123456" :debug)]
+      (is (= "123456" (:bot-token mybot)))
+      (is (= :debug (:log-level mybot))))))
