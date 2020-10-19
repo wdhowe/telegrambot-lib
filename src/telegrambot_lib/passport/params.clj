6604641b9bf2d1997289dcd3fd3d210da0dcdef4
wdhowe <wdhowe@protonmail.com>
2020-10-18 22:43:08 -0500

    Parameter records for payments namespace.
    Also corrected record name in passport params.

--- src/telegrambot_lib/passport/params.clj
+++ src/telegrambot_lib/passport/params.clj
@@ -2,7 +2,7 @@
   "Telegram Passport - parameter definitions."
   (:gen-class))
 
-(defrecord set-passport-data-errors
+(defrecord set-passport-data-errors-params
            [;; Required
             user_id ; user identifier
             errors ; json array of 'PassportElementError' describing the errors
