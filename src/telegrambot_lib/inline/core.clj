f5e3667f420191f86117cda1708ee9f7c1afaeff
wdhowe <wdhowe@protonmail.com>
2020-10-24 21:33:10 -0500

    issue/25/req-opt-params-inline-ns: Function req/optional parameters - inline namespace
    New multi-arity parameters for functions in the inline namespace.

--- src/telegrambot_lib/inline/core.clj
+++ src/telegrambot_lib/inline/core.clj
@@ -6,10 +6,22 @@
   (:require [telegrambot-lib.http :as http]))
 
 (defn answer-inline-query
-  "Use this method to send answers to an inline query. On success, True is returned.
+  "Use this method to send answers to an inline query.
+   On success, True is returned.
    No more than 50 results per query are allowed."
-  [this content]
-  (http/request this "answerInlineQuery" content))
+  ([this content]
+   (http/request this "answerInlineQuery" content))
+
+  ([this inline_query_id results]
+   (let [content {:inline_query_id inline_query_id
+                  :results results}]
+     (answer-inline-query this content)))
+
+  ([this inline_query_id results & optional]
+   (let [content (merge (first optional)
+                        {:inline_query_id inline_query_id
+                         :results results})]
+     (answer-inline-query this content))))
 
 (def behavior
   "Map for extending the core TBot record with functions."
