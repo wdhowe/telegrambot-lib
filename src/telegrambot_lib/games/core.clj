66d0c97e6674239f3066e7225bc941ddbfc1583f
wdhowe <wdhowe@protonmail.com>
2020-10-24 21:22:53 -0500

    issue/24/req-opt-params-games-ns: Function req/optional parameters - games namespace
    Added multi-arity parameters for req/opt params in the games namespace.

--- src/telegrambot_lib/games/core.clj
+++ src/telegrambot_lib/games/core.clj
@@ -7,26 +7,93 @@
 
 (defn send-game
   "Use this method to send a game. On success, the sent Message is returned."
-  [this content]
-  (http/request this "sendGame" content))
+  ([this content]
+   (http/request this "sendGame" content))
+
+  ([this chat_id game_short_name]
+   (let [content {:chat_id chat_id
+                  :game_short_name game_short_name}]
+     (send-game this content)))
+
+  ([this chat_id game_short_name & optional]
+   (let [content (merge (first optional)
+                        {:chat_id chat_id
+                         :game_short_name game_short_name})]
+     (send-game this content))))
 
 (defn set-game-score
   "Use this method to set the score of the specified user in a game.
    On success, if the message was sent by the bot, returns the edited Message,
    otherwise returns True. Returns an error, if the new score is not greater
    than the user's current score in the chat and force is False."
-  [this content]
-  (http/request this "setGameScore" content))
+  ([this content]
+   (http/request this "setGameScore" content))
+
+  ([this chat_id message_id user_id score]
+   (let [content {:chat_id chat_id
+                  :message_id message_id
+                  :user_id user_id
+                  :score score}]
+     (set-game-score this content)))
+
+  ([this chat_id message_id user_id score & optional]
+   (let [content (merge (first optional)
+                        {:chat_id chat_id
+                         :message_id message_id
+                         :user_id user_id
+                         :score score})]
+     (set-game-score this content))))
+
+(defn set-game-score-inline
+  "Use this method to set the inline score of the specified user in a game.
+   On success, if the message was sent by the bot, returns the edited Message,
+   otherwise returns True. Returns an error, if the new score is not greater
+   than the user's current score in the chat and force is False."
+  ([this content]
+   (http/request this "setGameScore" content))
+
+  ([this inline_message_id user_id score]
+   (let [content {:inline_message_id inline_message_id
+                  :user_id user_id
+                  :score score}]
+     (set-game-score-inline this content)))
+
+  ([this inline_message_id user_id score & optional]
+   (let [content (merge (first optional)
+                        {:inline_message_id inline_message_id
+                         :user_id user_id
+                         :score score})]
+     (set-game-score-inline this content))))
 
 (defn get-game-high-scores
   "Use this method to get data for high score tables.
    Will return the score of the specified user and several of their neighbors in a game.
    On success, returns an Array of GameHighScore objects."
-  [this content]
-  (http/request this "getGameHighScores" content))
+  ([this content]
+   (http/request this "getGameHighScores" content))
+
+  ([this chat_id message_id user_id]
+   (let [content {:chat_id chat_id
+                  :message_id message_id
+                  :user_id user_id}]
+     (get-game-high-scores this content))))
+
+(defn get-game-high-scores-inline
+  "Use this method to get data for inline high score tables.
+   Will return the score of the specified user and several of their neighbors in a game.
+   On success, returns an Array of GameHighScore objects."
+  ([this content]
+   (http/request this "getGameHighScores" content))
+
+  ([this inline_message_id user_id]
+   (let [content {:inline_message_id inline_message_id
+                  :user_id user_id}]
+     (get-game-high-scores-inline this content))))
 
 (def behavior
   "Map for extending the core TBot record with functions."
   {:send-game send-game
    :set-game-score set-game-score
-   :get-game-high-scores get-game-high-scores})
+   :set-game-score-inline set-game-score-inline
+   :get-game-high-scores get-game-high-scores
+   :get-game-high-scores-inline get-game-high-scores-inline})
