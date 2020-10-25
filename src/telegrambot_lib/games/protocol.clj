afce64cbebd3535d98589d18f8dc350a0101a173
wdhowe <wdhowe@protonmail.com>
2020-10-25 10:44:37 -0500

    issue/40/docstrings-games: records->docstrings - games ns
    - Converted params record to fn docstrings.
    - Updated protocol to match fn signatures.

--- src/telegrambot_lib/games/protocol.clj
+++ src/telegrambot_lib/games/protocol.clj
@@ -7,15 +7,34 @@
   "Bot API for HTML5 games on Telegram."
 
   (send-game [this content]
+    [this chat_id game_short_name]
+    [this chat_id game_short_name & optional]
     "Use this method to send a game. On success, the sent Message is returned.")
 
   (set-game-score [this content]
+    [this chat_id message_id user_id score]
+    [this chat_id message_id user_id score & optional]
+    "Use this method to set the score of the specified user in a game.
+     On success, if the message was sent by the bot, returns the edited Message,
+     otherwise returns True. Returns an error, if the new score is not greater
+     than the user's current score in the chat and force is False.")
+
+  (set-game-score-inline [this content]
+    [this inline_message_id user_id score]
+    [this inline_message_id user_id score & optional]
     "Use this method to set the score of the specified user in a game.
      On success, if the message was sent by the bot, returns the edited Message,
      otherwise returns True. Returns an error, if the new score is not greater
      than the user's current score in the chat and force is False.")
 
   (get-game-high-scores [this content]
+    [this chat_id message_id user_id]
+    "Use this method to get data for high score tables.
+     Will return the score of the specified user and several of their neighbors in a game.
+     On success, returns an Array of GameHighScore objects.")
+
+  (get-game-high-scores-inline [this content]
+    [this inline_message_id user_id]
     "Use this method to get data for high score tables.
      Will return the score of the specified user and several of their neighbors in a game.
      On success, returns an Array of GameHighScore objects."))
