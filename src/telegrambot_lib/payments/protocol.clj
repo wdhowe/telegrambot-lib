2bef238df75dff50e304a62828b986452444e8db
wdhowe <wdhowe@protonmail.com>
2020-10-25 22:44:33 -0500

    issue/44/docstrings-payments: records->docstrings - payments ns
    - Converted params record to fn docstrings.
    - Updated protocol to match fn signatures.

--- src/telegrambot_lib/payments/protocol.clj
+++ src/telegrambot_lib/payments/protocol.clj
@@ -7,17 +7,38 @@
   "Bots accepting payments from users."
 
   (send-invoice [this content]
+    [this chat_id title description payload provider_token
+     start_parameter currency prices]
+    [this chat_id title description payload provider_token
+     start_parameter currency prices & optional]
     "Use this method to send invoices.
      On success, the sent Message is returned.")
 
-  (answer-shipping-query [this content]
+  (answer-shipping-query-ok [this content]
+    [this shipping_query_id shipping_options]
     "If you sent an invoice requesting a shipping address
      and the parameter is_flexible was specified, the Bot API
      will send an Update with a shipping_query field to the bot.
      Use this method to reply to shipping queries.
      On success, True is returned.")
 
-  (answer-precheckout-query [this content]
+  (answer-shipping-query-error [this content]
+    [this shipping_query_id error_message]
+    "If you sent an invoice requesting a shipping address
+     and the parameter is_flexible was specified, the Bot API
+     will send an Update with a shipping_query field to the bot.
+     Use this method to reply to shipping queries.
+     On success, True is returned.")
+
+  (answer-precheckout-query-ok [this pre_checkout_query_id]
+    "Once the user has confirmed their payment and shipping details,
+     the Bot API sends the final confirmation in the form of an Update
+     with the field pre_checkout_query.
+     Use this method to respond to such pre-checkout queries.
+     On success, True is returned.")
+
+  (answer-precheckout-query-error [this content]
+    [this pre_checkout_query_id error_message]
     "Once the user has confirmed their payment and shipping details,
      the Bot API sends the final confirmation in the form of an Update
      with the field pre_checkout_query.
