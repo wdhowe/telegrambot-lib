0ee0b6f3b88613fab1086a7d136a9dd46e3ae201
Bill Howe <wdhowe@protonmail.com>
2023-11-02 19:23:40 -0500

    issue-127: cljdoc not building since 2.4.0
    - Minor update to project.clj comment.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   
-  ;;; Dependencies, Plugins - scope provided for api analysis with cljdoc.
+  ;;; Dependencies, Plugins
   :dependencies [[clj-http "3.12.3"]
                  [environ "1.2.0"]
                  [org.clojure/clojure "1.11.1"]
